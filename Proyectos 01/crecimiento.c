4a164008205480a5dbcbdaec76b7ec75eee56187
Davatec <davatecimport@gmail.com>
2023-03-30 17:23:52 -0500

    bibliotecas y carpeta crecimiento

---
+++ Proyectos 01/crecimiento.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>   
+
+int main(void)
+{         
+    // Por hacer: Solicitar cantidad inicial
+
+    // Por hacer: Solicitar cantidad final
+
+    // Por hacer: Calcular número de años hasta alcanzar el tamaño final
+
+    // Por hacer: Imprimir número de años o ciclos anuales
+
+}
